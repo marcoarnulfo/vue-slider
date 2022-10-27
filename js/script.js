c928a8e21cc71e6dce9ca8fbf8dbf5fb315eee6b
Marco Arnulfo <arnulfomarco7@gmail.com>
2022-10-27 16:08:01 +0200

    update carousel and @click

--- js/script.js
+++ js/script.js
@@ -13,34 +13,51 @@ createApp({
                     text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
                 },
                 {
-                    image: 'img/02.webp',
+                    image: './img/02.webp',
                     title: 'Ratchet & Clank: Rift Apart',
                     text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
                 },
                 {
-                    image: 'img/03.webp',
+                    image: './img/03.webp',
                     title: 'Fortnite',
                     text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
                 },
                 {
-                    image: 'img/04.webp',
+                    image: './img/04.webp',
                     title: 'Stray',
                     text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
                 },
                 {
-                    image: 'img/05.webp',
+                    image: './img/05.webp',
                     title: "Marvel's Avengers",
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                 }
-            ]   
+            ]
         }
+    },
+    methods: {
+        prevImage() {
+            this.immagineAttiva--
+            if (this.immagineAttiva < 0) {
+                this.immagineAttiva = this.slides.length - 1
+            }
+        },
+        nextImage() {
+            console.log('slide to next image');
+            this.immagineAttiva++
+            if (this.immagineAttiva === this.slides.length) {
+                this.immagineAttiva = 0
+            }
+        },
     }
 }).mount('#app')
 
 
 
 
-
+// // ${index === 0 ? 'active' : ""}
+// se index è uguale a 0 ? allora aggiungi "active"
+// : altrimenti "" (non aggiungere niente)
 
 
 
